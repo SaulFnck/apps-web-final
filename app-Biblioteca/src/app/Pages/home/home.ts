a90764edd9aa4ed6e391b483cf72a78d3e4e4102
SaulFnck <sauld5354@gmail.com>
2025-12-07 20:00:10 -0600

    feat: :zap: Add Function to count libros, clientes and rentas on dashboard

--- app-Biblioteca/src/app/Pages/home/home.ts
+++ app-Biblioteca/src/app/Pages/home/home.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LibrosService } from '../../services/libros/libros-service';
+import { ClientesService } from '../../services/clientes/clientes-service';
+import { RentasService } from '../../services/rentas/rentas-service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  totalLibros: number = 0;
+  totalClientes: number = 0;
+  totalRentas: number = 0;
+
+  constructor(private booksService: LibrosService,
+    private clientesService: ClientesService,
+    private rentasService: RentasService) {}
+
+  ngOnInit(): void {
+    this.booksService.getAllLibros().subscribe((data) => {
+      this.totalLibros = data.length;
+    });
+
+    this.clientesService.getAllClientes().subscribe((data) => {
+      this.totalClientes = data.length;
+    });
+
+    this.rentasService.getAllRentas().subscribe((data) => {
+      this.totalRentas = data.length;
+    });
+  }
+
+
+}
+
